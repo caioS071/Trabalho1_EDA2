84dded94176202f7e018ddcd83eebb35ff792f91
Caio Silva Pontes <71469892+caioS071@users.noreply.github.com>
2023-04-17 23:00:37 -0300

    Atualização final - Explicit

--- Explícito com uso de ponteiros/explicit.py
+++ Explícito com uso de ponteiros/explicit.py
@@ -1,10 +1,45 @@
+
+#-----------------------Função de Inserção-------------------------------
+def insertion(hashTable, point):
+#Variavés de controle
+	posLivre = len(hashTable)-1
+	qntInteiros = 1 
+	aux=0
+	
+#inserção
+	while(qntInteiros < len(inteiros)):
+		o=0
+		aux = int(inteiros[qntInteiros]%inteiros[0])
+
+		if hashTable[aux] == "vazio":
+			hashTable[aux] = int(inteiros[qntInteiros])
+
+		elif posLivre != -1:
+			hashTable[posLivre] = int(inteiros[qntInteiros])
+			#Atualização da tabela dos apontadores
+			while(o<1):
+				if point[aux] == "vazio":
+					point[aux] = int(posLivre)
+					o+=1
+				else:
+					aux = point[aux]
+			#Mudar o apontador para a próxima posição livre do arquivo		
+			while (hashTable[posLivre] != "vazio"):
+				posLivre -=1
+
+		else:
+			print("arquivo cheio!")
+
+		qntInteiros +=1
+
+#-----------------------Função Principal-------------------------------
 #Inserir o nome do .txt sem a extensão
 print("Insira o nome do arquivo .txt que será utilizado para efetuar o Hashing: ")
 nomeDoarquivo = input()
 nomeDoarquivo = nomeDoarquivo + ".txt"
 
 #Leitura do arquivo .txt
-with open(nomeDoarquivo, "r") as arquivo:
+with open("Trabalho1_EDA2/Explícito com uso de ponteiros/" + nomeDoarquivo, "r") as arquivo:
 	texto = arquivo.readlines()
 
 #inicialização das listas e variavéis que serão utilizadas
@@ -23,37 +58,16 @@ while (cont<inteiros[0]):
 	point.append("vazio")
 	cont +=1
 	
-#Variavés de controle
-posLivre = len(hashTable)-1
-qntInteiros = 1  
-aux=0
-j=0
 
-#inserção
-while(qntInteiros < len(inteiros)):
-	o=0
-	aux = int(inteiros[qntInteiros]%inteiros[0])
+insertion(hashTable, point)
 
-	if hashTable[aux] == "vazio":
-			hashTable[aux] = int(inteiros[qntInteiros])
+with open("Trabalho1_EDA2/Explícito com uso de ponteiros/saida.txt", "a") as arquivo:
+	arquivo.write("HashTable: \n" + str(hashTable))
 
-	elif posLivre != -1:
-			hashTable[posLivre] = int(inteiros[qntInteiros])
-			#Atualização da tabela dos apontadores
-			while(o<1):
-				if point[aux] == "vazio":
-					point[aux] = int(posLivre)
-					o+=1
-				else:
-					aux = point[aux]
-			#Mudar o apontador para a próxima posição livre do arquivo		
-			while (hashTable[posLivre] != "vazio"):
-				posLivre -=1
+with open("Trabalho1_EDA2/Explícito com uso de ponteiros/saida.txt", "a") as arquivo:
+	arquivo.write("\nApontadores: \n"+ str(point))
 
-	else:
-		print("arquivo cheio!")
 
-	qntInteiros +=1
 print("Tabela hashing após o processo:")
 print(hashTable)
 print("-----------------------------------------------------------------------")
